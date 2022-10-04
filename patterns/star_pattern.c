64b49ec662778c077b2435e6d5a4733e1533497b
Theadityaraj05 <rajaditya02520@gmail.com>
2022-10-05 00:16:35 +0530

    pattern_*

--- patterns/star_pattern.c
+++ patterns/star_pattern.c
@@ -1,8 +1,10 @@
 /*
-* 
-* *       
-* * *          
+* * * * * * 
+* * * * * 
 * * * * 
+* * * 
+* * 
+* 
 */
                
 
@@ -10,10 +12,12 @@
 int main()
 {
     int n;
+    printf("Enter the numbers of *");
     scanf("%d",&n);
-    for(int i=1;i>n+1;i++){
+    for(int i=n;i>0;i--){
         
-        for(int j=0;j<i;j++){
+        for(int j=i;j>0;j--)
+        {
             printf("* ");
         }
         printf("\n");
